38cde37b5b56fe487332b491ed99d1b91a94276f
Alex <alex.shch12@gmail.com>
2019-02-22 12:29:22 +0200

    2019-02-22 404err

--- wp-content/themes/underscores/404.php
+++ wp-content/themes/underscores/404.php
@@ -20,45 +20,15 @@ get_header();
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Внимания! Эта страница не найдена.', 'fotos' ); ?></h1>
-<!-- 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'fotos' ); ?></h1>
- -->				</header><!-- .page-header -->
+					<h1 class="page-title"><?php esc_html_e( 'Внимание! Эта страница не найдена.', 'fotos' ); ?></h1>
+					<hr style="opacity:0"; margin-top: 30px;>
+					<img src="<?php bloginfo('template_url'); ?>/images/404r.jpg" alt="">
+				</header><!-- .page-header -->
 
 				<div class="page-content">
 					<p><?php esc_html_e( 'Ничего не было найдено в этом месте. Попробуйте другой пункт меню или новый поиск.', 'fotos' ); ?></p>
-<!-- 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'fotos' ); ?></p>
+<!-- 					<img src="<?php bloginfo('template_url'); ?>/images/404r.jpg" alt="">
  -->
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Самые используемые категории', 'fotos' ); ?></h2>
-<!-- 						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'fotos' ); ?></h2>
- -->						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$fotos_archive_content = '<p>' . sprintf( esc_html__( 'Попробуйте поискать в месячных архивах. %1$s', 'fotos' ), convert_smilies( ':)' ) ) . '</p>';
-					// $fotos_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'fotos' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$fotos_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
--- wp-content/themes/underscores/sidebar.php
+++ wp-content/themes/underscores/sidebar.php
@@ -12,10 +12,8 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<!-- <div class="row"> -->
 <div class="col-lg-3 col-md-3">
 	<aside id="secondary" class="widget-area" >
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</aside><!-- #secondary -->
 </div>
-<!-- </div> !-- .row --> 
